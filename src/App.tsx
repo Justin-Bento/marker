f8f06d95d74152800687717e32fef787ab2ee1ff
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-10-18 10:55:44 -0400

    Adjusted typography based on tailwind styles.

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,22 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="scroll-m-20 capitalize pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Vite + React
+      </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="leading-7 font-mono"
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className="leading-[1.5] text-md capitalize">
+          Edit <code className="font-mono text-sm">src/App.tsx</code> and save
+          to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-sm text-gray-400">
         Click on the Vite and React logos to learn more
       </p>
     </>
