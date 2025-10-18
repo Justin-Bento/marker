7b8122c345f5844ff1f95537d2260be19bd19458
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-10-18 10:39:04 -0400

    Added tailwind plugin to vite config file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // ...
+  ],
   base: "./", // Crucial for relative paths in Chrome extensions
   build: {
     outDir: "dist",
