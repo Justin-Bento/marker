35b85fe7b844f1b3bb91f4d78b0f33882e7be890
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-10-18 10:59:20 -0400

    Updated card css to fit tailwind code and formated the code in-between.

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,20 @@ function App() {
       <h1 className="scroll-m-20 capitalize pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Vite + React
       </h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="leading-7 font-mono"
-        >
-          count is {count}
-        </button>
-        <p className="leading-[1.5] text-md capitalize">
-          Edit <code className="font-mono text-sm">src/App.tsx</code> and save
-          to test HMR
-        </p>
+      <div className="overflow-hidden sm:rounded-lg dark:bg-gray-800/50">
+        <div className="px-4 py-5 sm:p-6 space-y-4">
+          {/* Content goes here */}
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            className="leading-7 font-mono"
+          >
+            count is {count}
+          </button>
+          <p className="leading-[1.5] text-md capitalize">
+            Edit <code className="font-mono text-sm">src/App.tsx</code> and save
+            to test HMR
+          </p>
+        </div>
       </div>
       <p className="text-sm text-gray-400">
         Click on the Vite and React logos to learn more
