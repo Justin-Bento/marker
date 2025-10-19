4855faa6b5668bf2229c2f93e2955f54b8674c23
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-10-19 16:12:56 -0400

    Inserted a h1 and a p tag to store the extention title and purpose.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ export default function App() {
   return (
     <main className="">
       <div className="overflow-hidden rounded-lg bg-white shadow-sm dark:bg-gray-800/50 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/10">
-        <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
+        <div className="px-4 py-5 sm:p-6">
+          {/* Content goes here */}
+          <div className="">
+            <h1 className="">Lorem, ipsum dolor.</h1>
+            <p className="">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+              impedit placeat odit eaque. Facilis id quam error quaerat soluta.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
