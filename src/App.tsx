722a57c89c3ae1b3f4201207b6d3d380e737378a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-10-19 16:48:36 -0400

    Updated root element to have a max width and height

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <>
-      <div className="overflow-hidden rounded-lg bg-white shadow-sm dark:bg-gray-800/50 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/10">
+    <div className="w-full h-full grid place-content-center">
+      <div className="overflow-hidden rounded-lg dark:bg-gray-800/50 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/10">
         <div className="px-4 py-5 sm:p-6">
           {/* Content goes here */}
           <div className="">
@@ -26,6 +26,6 @@ export default function App() {
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
