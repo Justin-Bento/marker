2a21701e9223dd14010b2e747b309e6ef35119d3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 17:46:09 -0500

    Moved <main> and <section> to landing page for semantic structure

---
+++ src/frontend/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="">
+      <section className="">
+        <h1>Home Page</h1>
+        <p>Welcome!</p>
+      </section>
+    </main>
+  );
+}
