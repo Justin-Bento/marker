c265e982ff2fdd530b31684ae46c9204930f5adc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 17:38:17 -0500

    Add <main> and <section> to landing page for semantic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 export default function App() {
   return (
-    <div>App</div>
+    <main className="">
+      <section className="">
+        <h1>Home Page</h1>
+        <p>Welcome!</p>
+      </section>
+    </main>
   );
 }
