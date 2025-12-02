b731d8e2480fb518e86faa7a41cf4cd72aaece5d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 17:51:26 -0500

    Update import path for App.tsx after moving to frontend folder.

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./frontend/Home";
+import Home from "./Home";
 
 export default function App() {
   return (
