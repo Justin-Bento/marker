18edaa32644a81a6cd5ae38f4bf308bba8a52ee9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 17:53:05 -0500

    Update import path for index.css after moving to frontend folder.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './frontend/index.css'
 import App from './frontend/App'
 
 createRoot(document.getElementById('root')!).render(
