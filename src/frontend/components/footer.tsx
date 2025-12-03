8078218898770da204bf8c63ac0db9c218dae979
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-03 12:51:46 -0500

    Turned the logo into the link for the user to return home.

--- src/frontend/components/footer.tsx
+++ src/frontend/components/footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <footer className="bg-black *:text-white">
       <section className="wrapper py-8">
+       <a href="/">
         <img src={CompanyLogo} className="w-32 m-0 p-0" />
+      </a>
       </section>
     </footer>
   );
--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-90 bg-white shadow-sm border-b border-primary-950/10">
       <section className="wrapper py-8 grid grid-cols-2 items-center">
+      <a href="/">
         <img src={CompanyLogo} className="w-32 m-0 p-0" />
+      </a>
         <nav className="">
           <ul className="flex items-center justify-end gap-4">
             {linksToWebpage.map((link: string, index:number) => {
