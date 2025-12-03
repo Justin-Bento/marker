4b18d7af58774e9a543033e21df69ab89e626814
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 22:03:02 -0500

    Added shadow and border to elevate header elemnets above the content

--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-white">
+    <header className="bg-white shadow-sm border-b border-primary-950/10">
       <section className="wrapper">
         <p>header</p>
       </section>
