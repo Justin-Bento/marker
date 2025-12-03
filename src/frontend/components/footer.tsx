1867235316f29f2d7efba018a480a087337ef660
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 22:05:28 -0500

    Added spacing inside the footer component and ensured text is legible.

--- src/frontend/components/footer.tsx
+++ src/frontend/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-black">
-      <section className="wrapper">
+    <footer className="bg-black *:text-white">
+      <section className="wrapper py-8">
         <p>footer</p>
       </section>
     </footer>
