0cde5ba525799c1c0c109cbac454cdb84028ba09
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-03 12:36:41 -0500

    Looped the linksToWebpage.

--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -1,17 +1,22 @@
 import CompanyLogo from "../../../public/Marker_RGB_Black.svg";
 
 export default function Header() {
-   const linksToWebpage = [
-    "home",
-    "about",
-    "tutorials",
-    "contact",
-    "support"
-  ]
+  const linksToWebpage = ["home", "about", "tutorials", "contact", "support"];
   return (
     <header className="sticky top-0 z-90 bg-white shadow-sm border-b border-primary-950/10">
       <section className="wrapper py-8">
         <img src={CompanyLogo} className="w-32 m-0 p-0" />
+        <nav className="">
+          <ul className="">
+            {linksToWebpage.map((link: string, index:number) => {
+              return (
+                <li key={index} className="">
+                  <a href={`/${link.toLocaleLowerCase()}`} className="capitalize">{link}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </section>
     </header>
   );
