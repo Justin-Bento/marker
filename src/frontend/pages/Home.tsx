42fde62bb30bbd4918c7008e3065bbfc16e5b48e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 20:02:22 -0500

    Added content from figma file.

--- src/frontend/pages/Home.tsx
+++ src/frontend/pages/Home.tsx
@@ -2,8 +2,55 @@ export default function Home() {
   return (
     <main className="wrapper min-h-dvh">
       <section className="">
-        <h1>Home Page</h1>
-        <p>Welcome!</p>
+        <h1>It’s Easy to Lose Sight of Your Project’s Vision</h1>
+        <p>
+          Projects get paused. Plans change. Life interrupts. When you return,
+          it’s hard to remember your steps or your purpose—and most project
+          tools don’t make that any easier. This tracker helps you rebuild focus
+          and get back on track effortlessly.
+        </p>
+      </section>
+      <section className="">
+        <h2>A Master Document That Works With You</h2>
+        <p>
+          Traditional project trackers assume you already have all the details.
+          This one doesn’t. It guides you step-by-step, helping you think,
+          write, and shape your project as it develops..
+        </p>
+      </section>
+      <section className="">
+        <h3>It’s Easy to Lose Sight of Your Project’s Vision</h3>
+        <p>
+          Projects get paused. Plans change. Life interrupts. When you return,
+          it’s hard to remember your steps or your purpose—and most project
+          tools don’t make that any easier. This tracker helps you rebuild focus
+          and get back on track effortlessly.
+        </p>
+      </section>
+      <section className="">
+        <h4>Don’t have Microsoft Office? no problem </h4>
+        <p>
+          You can open the Project Tracker in over 10 compatible formats. Google
+          Docs, Word Online, Apple Pages, Libre Office and more. Use it on any
+          device, only without installing extra software.
+        </p>
+      </section>
+      <section className="">
+        <h5>you are the project planner!</h5>
+        <p>
+          At its core, this is a simple Word document. The structure is there to
+          support you, but you are the planner who brings the project to life. I
+          built this website to show how even one small idea can grow into
+          something bigger—just like the site you’re on right now..
+        </p>
+      </section>
+      <section className="">
+        <h6>Here’s what other people have to say about</h6>
+        <p>
+          You can open the Project Tracker in over 10 compatible formats. Google
+          Docs, Word Online, Apple Pages, Libre Office and more. Use it on any
+          device, only without installing extra software..
+        </p>
       </section>
     </main>
   );
