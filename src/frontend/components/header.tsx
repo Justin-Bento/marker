2144b0a3ec9e1bc6ca6dc4a840bc4ea69563c38e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-03 12:37:48 -0500

    Shifted links into a line and jistifyed items to the end

--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -4,10 +4,10 @@ export default function Header() {
   const linksToWebpage = ["home", "about", "tutorials", "contact", "support"];
   return (
     <header className="sticky top-0 z-90 bg-white shadow-sm border-b border-primary-950/10">
-      <section className="wrapper py-8">
+      <section className="wrapper py-8 grid grid-cols-2 items-center">
         <img src={CompanyLogo} className="w-32 m-0 p-0" />
         <nav className="">
-          <ul className="">
+          <ul className="flex items-center justify-end gap-4">
             {linksToWebpage.map((link: string, index:number) => {
               return (
                 <li key={index} className="">
