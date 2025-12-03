a8d6f522be7ac44cbc1b144e28379ab0af4e6831
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 22:03:48 -0500

    Applied sticky position to the header to ensure the element sticks to the top of the application.

--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-white shadow-sm border-b border-primary-950/10">
+    <header className="sticky top-0 z-90 bg-white shadow-sm border-b border-primary-950/10">
       <section className="wrapper">
         <p>header</p>
       </section>
