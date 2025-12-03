050da7c1030e5008b884bd643e2bf6abd5615b5c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-02 22:04:26 -0500

    Added spacing inside the header component of 32px

--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="sticky top-0 z-90 bg-white shadow-sm border-b border-primary-950/10">
-      <section className="wrapper">
+      <section className="wrapper py-8">
         <p>header</p>
       </section>
     </header>
