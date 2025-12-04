1c437be1bebadf2e9f4f4ce9ec621ffd0d521b5b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-04 11:59:11 -0500

    Importated top-level pages

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import About from "./pages/About";
+import Tutorials from "./pages/Tutorials";
+import Contact from "./pages/Contact";
+import Support from "./pages/Support";
 
 export default function App() {
   return (
@@ -11,6 +15,10 @@ export default function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/tutorials" element={<Tutorials />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/support" element={<Support />} />
           </Routes>
         </Router>
       </main>
