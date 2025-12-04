17d47fb75fd2365134827e97b6e66df58e6ad7d4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-04 11:03:22 -0500

    Formatted document using prettier.

--- src/frontend/pages/Home.tsx
+++ src/frontend/pages/Home.tsx
@@ -2,22 +2,31 @@ export default function Home() {
   return (
     <>
       <section className="py-20">
-        <h1 className="md:text-6xl max-w-[20ch] text-balance leading-20 text-left capitalize">It’s Easy to Lose Sight of Your Project’s Vision</h1>
+        <h1 className="md:text-6xl max-w-[20ch] text-balance leading-20 text-left capitalize">
+          It’s Easy to Lose Sight of Your Project’s Vision
+        </h1>
         <p className="max-w-[80ch] text-pretty text-xl leading-10 mt-4 mb-12">
           Projects get paused. Plans change. Life interrupts. When you return,
           it’s hard to remember your steps or your purpose—and most project
           tools don’t make that any easier. This tracker helps you rebuild focus
           and get back on track effortlessly.
         </p>
-          <a href="https://docs.google.com/document/d/1PuBIeAT3HBt4--GWk0KAV9K6Uo6pJJnQLcDqJNUgPN0/edit?usp=sharing" target="_blank" className="bg-primary-400 px-6 py-3 rounded-md capitalize hover:underline hover:cursor-pointer">
-            Download Template Today
-          </a>
-        </section>
+        <a
+          href="https://docs.google.com/document/d/1PuBIeAT3HBt4--GWk0KAV9K6Uo6pJJnQLcDqJNUgPN0/edit?usp=sharing"
+          target="_blank"
+          className="bg-primary-400 px-6 py-3 rounded-md capitalize hover:underline hover:cursor-pointer"
+        >
+          Download Template Today
+        </a>
+      </section>
       <section className="">
-        <h2 className="landing-headline-laptop">A Document That Works With You!</h2>
+        <h2 className="landing-headline-laptop">
+          A Document That Works With You!
+        </h2>
         <p className="landing-copy-laptop">
-          Traditional project trackers assume you already have all the details. 
-          This one doesn’t. It gives you room to think, write, and shape your project as it develops.
+          Traditional project trackers assume you already have all the details.
+          This one doesn’t. It gives you room to think, write, and shape your
+          project as it develops.
         </p>
         <div className="my-12">
           <p>What's Inside The Document?</p>
@@ -33,9 +42,12 @@ export default function Home() {
         <p className="">Remember: This is for your eyes only.</p>
       </section>
       <section className="">
-        <h3 className="landing-headline-laptop">Don’t have Microsoft Office? no problem </h3>
+        <h3 className="landing-headline-laptop">
+          Don’t have Microsoft Office? no problem{" "}
+        </h3>
         <p className="landing-copy-laptop">
-          It's just a document that on the comptuer and is free for you to use and share. Here are some software I'd reccmend for the best expeeince 
+          It's just a document that on the comptuer and is free for you to use
+          and share. Here are some software I'd reccmend for the best expeeince
         </p>
         <div className="my-12">
           <p>Software to Download based on your local system</p>
@@ -57,7 +69,9 @@ export default function Home() {
         </div>
       </section>
       <section className="">
-        <h4 className="landing-headline-laptop">you are the project planner!</h4>
+        <h4 className="landing-headline-laptop">
+          you are the project planner!
+        </h4>
         <p className="landing-copy-laptop">
           At its core, this is a simple Word document. The structure is there to
           support you, but you are the planner who brings the project to life. I
@@ -66,7 +80,9 @@ export default function Home() {
         </p>
       </section>
       <section className="">
-        <h5 className="landing-headline-laptop">Here’s what other people have to say about</h5>
+        <h5 className="landing-headline-laptop">
+          Here’s what other people have to say about
+        </h5>
         <p className="landing-copy-laptop">
           You can open the Project Tracker in over 10 compatible formats. Google
           Docs, Word Online, Apple Pages, Libre Office and more. Use it on any
