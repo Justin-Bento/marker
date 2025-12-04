258319e018a4c405f97d791e0f3afd45ae43eff9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-04 13:42:56 -0500

    Adjusted the grid layout for mobile, tablet and desktop.

--- src/frontend/components/header.tsx
+++ src/frontend/components/header.tsx
@@ -4,24 +4,29 @@ export default function Header() {
   const linksToWebpage = ["home", "about", "tutorials", "contact", "support"];
   return (
     <header className="sticky top-0 z-90 bg-white shadow-sm border-b border-primary-950/10">
-      <section className="wrapper py-8 grid grid-cols-2 items-center">
-      <a href="/">
-        <img src={CompanyLogo} className="w-32 m-0 p-0" />
-      </a>
-        <nav className="">
-          <ul className="flex items-center justify-end gap-4">
-            {linksToWebpage.map((link: string, index:number) => {
+      <div className="wrapper py-8 grid grid-cols-2 gap-4 items-center">
+        <div className="col-span-2 lg:col-span-1 flex items-center justify-between">
+          <a href="/">
+            <img src={CompanyLogo} className="w-32 m-0 p-0" />
+          </a>
+          <p className="inline-block lg:hidden">menu</p>
+        </div>
+        <nav className="col-span-2 hidden lg:col-span-1 lg:flex lg:items-center lg:justify-end">
+          <ul className="flex items-center justify-between gap-4">
+            {linksToWebpage.map((link: string, index: number) => {
               const isHome = link.toLowerCase() === "home";
-              const href = isHome ? '/' : `/${link.toLowerCase()}`;
+              const href = isHome ? "/" : `/${link.toLowerCase()}`;
               return (
                 <li key={index} className="">
-                  <a href={href} className="capitalize hover:underline">{link}</a>
+                  <a href={href} className="capitalize hover:underline">
+                    {link}
+                  </a>
                 </li>
               );
             })}
           </ul>
         </nav>
-      </section>
+      </div>
     </header>
   );
 }
