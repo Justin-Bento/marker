97c86d46a30d3fb2834999ecc56753a4d3b9f9ed
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-04 13:22:51 -0500

    Updated column.

--- src/frontend/components/footer.tsx
+++ src/frontend/components/footer.tsx
@@ -6,9 +6,13 @@ export default function Footer() {
       <div className="wrapper py-12">
         <div className="grid lg:grid-cols-5">
           <div className="col-span-2">
-            <a href="/">
+            <a href="/" className="block">
               <img src={CompanyLogo} className="w-32 m-0 p-0" />
             </a>
+            <p className="mt-6 tex-sm leading-7 tracking-wide">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus, cupiditate. Dolorum voluptatibus laborum.
+            </p>
           </div>
           {/*  */}
           <div className="flex flex-col items-end *:text-right">
