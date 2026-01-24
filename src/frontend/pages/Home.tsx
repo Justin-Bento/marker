ed683fc52430610df08cecb9c338011c916dfacc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2026-01-24 10:09:16 -0500

    fix(html): convert commas and double commas to HTML entities

--- src/frontend/pages/Home.tsx
+++ src/frontend/pages/Home.tsx
@@ -3,13 +3,13 @@ export default function Home() {
     <>
       <section className="py-20">
         <h1 className="md:text-6xl max-w-[20ch] text-balance leading-20 text-left capitalize">
-          It’s Easy to Lose Sight of Your Project’s Vision
+          It&rsquo;s Easy to Lose Sight of Your Project&rsquo;s Vision
         </h1>
         <p className="max-w-[80ch] text-pretty text-xl leading-10 mt-4 mb-12">
           Projects get paused. Plans change. Life interrupts. When you return,
-          it’s hard to remember your steps or your purpose—and most project
-          tools don’t make that any easier. This tracker helps you rebuild focus
-          and get back on track effortlessly.
+          it&rsquo;s hard to remember your steps or your purpose&mdash;and most
+          project tools don&rsquo;t make that any easier. This tracker helps you
+          rebuild focus and get back on track effortlessly.
         </p>
         <a
           href="https://docs.google.com/document/d/1PuBIeAT3HBt4--GWk0KAV9K6Uo6pJJnQLcDqJNUgPN0/edit?usp=sharing"
@@ -25,8 +25,8 @@ export default function Home() {
         </h2>
         <p className="landing-copy-laptop">
           Traditional project trackers assume you already have all the details.
-          This one doesn’t. It gives you room to think, write, and shape your
-          project as it develops.
+          This one doesn&rsquo;t. It gives you room to think, write, and shape
+          your project as it develops.
         </p>
         <div className="my-12">
           <p>What's Inside The Document?</p>
@@ -43,12 +43,13 @@ export default function Home() {
       </section>
       <section className="">
         <h3 className="landing-headline-laptop">
-          Don’t have Microsoft Office?
+          Don&rsquo;t have Microsoft Office?
         </h3>
         <p className="landing-copy-laptop">
-          No problem! It’s just a digital document, and you can open it with any
-          word processor. Plus, it’s completely free for you to use and share
-          with your friends. If you find it helpful share your story.
+          No problem! It&rsquo;s just a digital document, and you can open it
+          with any word processor. Plus, it&rsquo;s completely free for you to
+          use and share with your friends. If you find it helpful share your
+          story.
         </p>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 mt-12 gap-12">
@@ -83,12 +84,12 @@ export default function Home() {
           At its core, this is a simple Word document. The structure is there to
           support you, but you are the planner who brings the project to life. I
           built this website to show how even one small idea can grow into
-          something bigger—just like the site you’re on right now..
+          something bigger&mdash;just like the site you&rsquo;re on right now..
         </p>
       </section>
       <section className="">
         <h5 className="landing-headline-laptop">
-          Here’s what other people have to say about
+          Here&rsquo;s what other people have to say about
         </h5>
         <p className="landing-copy-laptop">
           You can open the Project Tracker in over 10 compatible formats. Google
