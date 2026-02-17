1c4860fda243bb886275eddb356263f356206fe4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2026-02-17 16:31:03 -0500

    refactor(layout): update contact page to include contact info + form

---
+++ src/frontend/components/contactform.tsx
@@ -0,0 +1,100 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  fullName: string;
+  exampleRequired: string;
+};
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("fullName")); // watch input value by passing the name of it
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+      {/* register your input into the hook by invoking the "register" function */}
+      <div>
+        <label
+          for="fullName"
+          class="block text-sm/6 font-medium text-gray-900 dark:text-white"
+        >
+          Full Name
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            placeholder="eg: john doe"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-amber-500"
+            {...register("fullName")}
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          for="email"
+          class="block text-sm/6 font-medium text-gray-900 dark:text-white"
+        >
+          Email
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            placeholder="john-doe@example.com"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-amber-500"
+            {...register("fullName")}
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          for="Subject"
+          class="block text-sm/6 font-medium text-gray-900 dark:text-white"
+        >
+          Subject
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            placeholder="eg: Having an a Pizza Party."
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-amber-500"
+            {...register("fullName")}
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          for="message"
+          class="block text-sm/6 font-medium text-gray-900 dark:text-white"
+        >
+          Your Message
+        </label>
+        <div className="mt-2">
+          <textarea
+            id="email"
+            rows={4}
+            placeholder="eg: John is invited to Janes pizza party will have fun entertainment on February 17th 2025."
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-amber-500"
+            {...register("fullName")}
+          />
+        </div>
+      </div>
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input
+        type="submit"
+        className="rounded-sm bg-gray-6 px-2 py-1 text-sm font-semibold text-white shadow-xs hover:bg-gray-5 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-6 dark:bg-gray-5 dark:shadow-none dark:hover:bg-gray-4 dark:focus-visible:outline-gray-5 hover:cursor-pointer"
+      />
+    </form>
+  );
+}
--- src/frontend/pages/Contact.tsx
+++ src/frontend/pages/Contact.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import ContactForm from "../components/contactform";
 
 export default function Contact() {
   useEffect(() => {
@@ -6,9 +7,33 @@ export default function Contact() {
   }, []);
   return (
     <>
-      <section className="mt-32">
-        <h1 className="">Welcome To The Contact Page.</h1>
-      </section>
+      <article className="grid md:grid-cols-2 gap-10 mx-auto mt-16">
+        <section className="">
+          <div>
+            <h2 className="font-medium text-2xl text-gray-800">
+              Contact Marker
+            </h2>
+            <p className="text-lg leading-relaxed text-slate-500 mt-3">
+              Have something to say? We are here to help. Fill up the form or
+              send email or call phone.
+            </p>
+            <div className="mt-5">
+              <div className="flex items-center mt-2 space-x-2 text-gray-600">
+                <span>1734 Sanfransico, CA 93063</span>
+              </div>
+              <div className="flex items-center mt-2 space-x-2 text-gray-600">
+                <a href="mailto:hello@astroshipstarter.com">hello@marker.com</a>
+              </div>
+              <div className="flex items-center mt-2 space-x-2 text-gray-600">
+                <a href="tel:+1 (987) 4587 899">+1 (987) 4587 899</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <ContactForm />
+        </section>
+      </article>
     </>
   );
 }
