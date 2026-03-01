53b48f8b446bf9de885523eb055081e3dda9cde0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2026-02-28 19:06:58 -0500

    chore(files): update file to include one global css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './frontend/index.css'
-import App from './frontend/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./frontend/global.css";
+import App from "./frontend/App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
