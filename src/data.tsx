2455025268a36c56d6c2466c042449873dfb7183
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2026-03-10 11:19:00 -0400

    core(content): remove boilerplate

--- src/data.tsx
+++ src/data.tsx
@@ -4,14 +4,6 @@ import {
   HiOutlineViewGrid,
 } from "react-icons/hi";
 
-export const MobyDick = [
-  "Loomings.",
-  "The Carpet-Bag.",
-  "The Spouter-Inn",
-  "The Counterpane.",
-  "Breakfast.",
-  "The Street.",
-];
 // Thanks project gutenberg: https://www.gutenberg.org/cache/epub/2701/pg2701.txt
 
 export const values = [
